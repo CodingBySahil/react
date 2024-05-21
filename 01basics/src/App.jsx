800ce534b070e9ded7a6ac627efe363c8f3791a6
codingBySahil <muhammadsahil345737@gmail.com>
2024-05-21 17:53:58 +0500

    simple CV

--- 01basics/src/App.jsx
+++ 01basics/src/App.jsx
@@ -1,4 +1,5 @@
 import myProfilePic from '../../images/myPic.jpeg'
+import MyDetails from './components/MyDetails'
 function App() {
   
   return (
@@ -7,6 +8,7 @@ function App() {
      <img src={myProfilePic} width={400}/>
      <h1>I am Muhammad Sahil</h1>
      <p>I am from peshawar student of BSCS in University of Peshawar</p>
+     <MyDetails/>
     </>
   )
 }
---
+++ 01basics/src/components/MyDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MyDetails() {
+  return (
+    <>
+    <h3>I am Learning React In SMIT with Sir AQLEEM ORAKZAI</h3>
+    <h3>My Name is Muhammad Sahil</h3>
+    <h3>Roll No : 159923 </h3>
+    </>
+  )
+}
+
+export default MyDetails
