8827d225ca28e8c557bd8a7fc61c125627873161
codingBySahil <muhammadsahil345737@gmail.com>
2024-05-21 17:14:34 +0500

    a very first simple CV in react

--- 01basics/src/App.jsx
+++ 01basics/src/App.jsx
@@ -1,8 +1,12 @@
+import myProfilePic from '../../images/myPic.jpeg'
 function App() {
   
   return (
     <>
-     <h1>Starting with the name of Allah Almighty who is the king of all kings</h1>
+     <h4>Starting with the name of Allah Almighty who is the king of all kings</h4>
+     <img src={myProfilePic} width={400}/>
+     <h1>I am Muhammad Sahil</h1>
+     <p>I am from peshawar student of BSCS in University of Peshawar</p>
     </>
   )
 }
