cee8ebd87478441238247ab2f3412b6e0a3d3128
codingBySahil <muhammadsahil345737@gmail.com>
2024-05-22 19:50:38 +0500

    try to clone youtube just basic vite setup ready

---
+++ 02YoutubeClone/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+     <h1>allah hu akbar</h1>
+     <p>I am going to create youtube clone by myself in react vite by using bootstrap</p>
+    </>
+  )
+}
+
+export default App
