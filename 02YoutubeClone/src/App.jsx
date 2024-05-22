3eb6bbdbf6d48099deca3073810df1801a97569f
codingBySahil <muhammadsahil345737@gmail.com>
2024-05-22 22:55:35 +0500

    images are added and main conatiner div is created

--- 02YoutubeClone/src/App.jsx
+++ 02YoutubeClone/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import ContainerFluid from './components/containerfluid'
+
 
 function App() {
 
   return (
     <>
-     <h1>allah hu akbar</h1>
+     <h1 >allah hu akbar</h1>
      <p>I am going to create youtube clone by myself in react vite by using bootstrap</p>
+     <ContainerFluid />
     </>
   )
 }
---
+++ 02YoutubeClone/src/components/containerfluid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContainerFluid() {
+  return (
+    <div className={'container-fluid'}>
+        <h5>I am fluid</h5>
+    </div>
+  )
+}
+
+export default ContainerFluid
