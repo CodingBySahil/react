332893a5861034e20836036925e8d098ae1048cf
codingBySahil <muhammadsahil345737@gmail.com>
2024-05-26 14:46:25 +0500

    all error are removed and custom and bootstrapp css and fontawesome are added

--- 02YoutubeClone/src/components/Navbar.jsx
+++ 02YoutubeClone/src/components/Navbar.jsx
@@ -1,48 +1,30 @@
 import React from "react";
-import "./navbar.css";
+
+
 const Navbar = () => {
   return (
-    <div className={"position-fixed w-100 top-0 mb-1"} id="searchbar-wrapper" style={{
-      backgroundColor: 'white',
-      zIndex: 1
-    }}> 
-    
-      <div className={"row"}>
-        <div className={"col-2"}>
-          <div className={"d-flex justify-content-start align-items-center"}>
-            <div>
-              <i className={"bi bi-list"} id="list-icon"></i>
+    <div className="position-fixed w-100 top-0 mb-1" style={{ backgroundColor: "white", zIndex: 1 }} id="searchbar-wrapper">
+    {/* search bar wrapper*/}
+    <div className="row">
+        <div className="col-2">
+            <div className="d-flex justify-content-start align-items-center">
+                <div><i className="bi bi-list" id="list-icon"></i></div>
+                <div><i className="bi bi-youtube text-danger"></i></div>
+                <div><span className="logo">YouTube<sup>PK</sup></span></div>
             </div>
-            <div>
-              <i className={"bi bi-youtube text-danger"}></i>
-            </div>
-            <div>
-              <span className={"logo"}>
-                YouTube<sup>PK</sup>
-              </span>
-            </div>
-          </div>
         </div>
-        <div className={"col-7"}>
-          <form className={"form-inline my-2 my-lg-0 d-flex search-box"}>
-            <input
-              className={"form-control mr-sm-2"}
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className={"btn my-2 my-sm-0"} type="submit">
-              <i className={"bi bi-search text-dark"}></i>
-            </button>
-          </form>
+        <div className="col-7">
+            <form className="form-inline my-2 my-lg-0 d-flex search-box">
+                <input className="form-control mr-sm-2 " type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn my-2 my-sm-0" type="submit"><i className="bi bi-search text-dark"></i></button>
+            </form>
         </div>
-        <div
-          className={"col-3   d-flex justify-content-center align-items-center"}
-        >
-          <p className={"m-1"}>Designed By codingBySAHIL</p>
+        <div className="col-3 d-flex justify-content-center align-items-center">
+            <p className="m-1">Designed By codingBySAHIL</p>
         </div>
-      </div>
     </div>
+</div>
+
   );
 };
 
