9fa3672605e158f35b01361dd9bcc97e5ee1fafd
codingBySahil <muhammadsahil345737@gmail.com>
2024-05-28 15:36:40 +0500

    style added

--- 03counter/src/App.jsx
+++ 03counter/src/App.jsx
@@ -3,19 +3,34 @@ import React, { useState } from "react";
 const App = () => {
   let [counter, setCounter] = useState(4);
   return (
-    <div
-      style={{
-        width: "50%",
-        border: "1px solid red",
-        backgroundColor: "black",
-        color: "white",
-        margin: "auto",
-      }}
-    >
-      <h3>{counter}</h3>
-      <button onClick={() => setCounter(counter + 1)}>Click to Add 1</button>
-      <button onClick={() => setCounter(counter - 1)}>Click to Subtract 1</button>
-    </div>
+    <>
+      <div class="card w-50 container mt-5 ">
+        <div class="card-body">
+          <h1 class="card-title">{counter}</h1>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg m-2"
+            onClick={() => setCounter(counter + 1)}
+          >
+            Click to Add 1
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg m-2"
+            onClick={() => setCounter(counter - 1)}
+          >
+            Click to Subtract 1
+          </button>
+        </div>
+        <h3 class="card-text">
+          You Recite{" "}
+          <h1 className="d-inline">
+            <b>{counter}</b>
+          </h1>{" "}
+          times
+        </h3>
+      </div>
+    </>
   );
 };
 
