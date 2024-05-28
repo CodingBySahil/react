ed056bda73ed84f52be3b33395605df631e5779c
codingBySahil <muhammadsahil345737@gmail.com>
2024-05-28 15:38:39 +0500

    style changed

--- 03counter/src/App.jsx
+++ 03counter/src/App.jsx
@@ -1,37 +1,42 @@
 import React, { useState } from "react";
 
+
 const App = () => {
-  let [counter, setCounter] = useState(4);
+  const [counter, setCounter] = useState(4);
+
   return (
-    <>
-      <div class="card w-50 container mt-5 ">
-        <div class="card-body">
-          <h1 class="card-title">{counter}</h1>
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="card w-50 text-center shadow-lg p-3 mb-5 bg-white rounded">
+        <div className="card-body">
+          <h1 className="card-title display-1">{counter}</h1>
           <button
             type="button"
-            className="btn btn-primary btn-lg m-2"
+            className="btn btn-success btn-lg m-2"
             onClick={() => setCounter(counter + 1)}
           >
             Click to Add 1
           </button>
           <button
             type="button"
-            className="btn btn-primary btn-lg m-2"
+            className="btn btn-danger btn-lg m-2"
             onClick={() => setCounter(counter - 1)}
           >
             Click to Subtract 1
           </button>
         </div>
-        <h3 class="card-text">
-          You Recite{" "}
-          <h1 className="d-inline">
-            <b>{counter}</b>
-          </h1>{" "}
-          times
-        </h3>
+        <div className="card-footer bg-transparent">
+          <h3 className="card-text">
+            You Recite{" "}
+            <h1 className="d-inline">
+              <b>{counter}</b>
+            </h1>{" "}
+            times
+          </h3>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
 export default App;
+
