6c65f666faf4396a749ed3bcd4167709df1c34b4
codingBySahil <muhammadsahil345737@gmail.com>
2024-07-22 11:41:37 +0500

    styled ubdated

--- 01basics/src/App.jsx
+++ 01basics/src/App.jsx
@@ -1,16 +1,19 @@
-import myProfilePic from '../../images/myPic.jpeg'
-import MyDetails from './components/MyDetails'
+import React from 'react';
+import myProfilePic from '../../images/myPic.jpeg';
+import MyDetails from './components/MyDetails';
+
 function App() {
-  
   return (
-    <>
-     <h4>Starting with the name of Allah Almighty who is the king of all kings</h4>
-     <img src={myProfilePic} width={400}/>
-     <h1>I am Muhammad Sahil</h1>
-     <p>I am from peshawar student of BSCS in University of Peshawar</p>
-     <MyDetails/>
-    </>
-  )
+    <div className="container mx-auto p-4">
+      <h4 className="text-xl font-bold text-gray-700 mb-4">Starting with the name of Allah Almighty who is the king of all kings</h4>
+      <div className="flex flex-col items-center mb-6">
+        <img src={myProfilePic} width={400} className="rounded-full shadow-lg mb-4" alt="My Profile Pic" />
+        <h1 className="text-3xl font-bold text-gray-900">I am Muhammad Sahil</h1>
+        <p className="text-gray-600">I am from Peshawar, student of BSCS in University of Peshawar</p>
+      </div>
+      <MyDetails />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- 01basics/src/components/MyDetails.jsx
+++ 01basics/src/components/MyDetails.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 function MyDetails() {
   return (
-    <>
-    <h3>I am Learning React In SMIT with Sir AQLEEM ORAKZAI</h3>
-    <h3>My Name is Muhammad Sahil</h3>
-    <h3>Roll No : 159923 </h3>
-    </>
-  )
+    <div className="p-4 bg-gray-100 rounded-md shadow-md">
+      <h3 className="text-lg font-semibold text-blue-500">I am Learning React In SMIT with Sir AQLEEM ORAKZAI</h3>
+      <h3 className="text-lg font-semibold text-blue-500">My Name is Muhammad Sahil</h3>
+      <h3 className="text-lg font-semibold text-blue-500">Roll No : 159923</h3>
+    </div>
+  );
 }
 
-export default MyDetails
+export default MyDetails;
